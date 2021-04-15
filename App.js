7d5bcb92c2f604fc45bf640ec843a48255be5b92
cdodd2 <Chris@Christophers-MacBook-Pro-2.local>
2021-04-15 13:05:49 -0400

    first front-end skeleton

--- App.js
+++ App.js
@@ -1,21 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, View, Text, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+function LoginScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Login to Spotify</Text>
+      <TextInput
+        style={{height: 40}}
+        placeholder="Username"
+        defaultValue={''}
+      />
+      <TextInput
+        style={{height: 40}}
+        placeholder="Password"
+        defaultValue={''}
+      />
+      <Button
+        title="Login"
+        onPress={() => navigation.navigate('Playlists')}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function PlaylistScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Playlists Available</Text>
+      <Button
+        title="Select Playlists"
+        onPress={() => navigation.navigate('PlaylistName')}
+      />
+    </View>
+  );
+}
+
+function PlaylistNameScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Name your new playlist:</Text>
+      <TextInput
+        style={{height: 40}}
+        placeholder="Playlist Name"
+        defaultValue={''}
+      />
+      <Button
+        title="Enter Name"
+        onPress={() => navigation.navigate('Finish')}
+      />
+    </View>
+  );
+}
+
+function FinishScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Playlist Created</Text>
+      <Text>Open Spotify to see your new playlist</Text>
+      <Button
+        title="Create Another Playlist"
+        onPress={() => navigation.navigate('Playlists')}
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" headerMode="none" mode="modal">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Playlists" component={PlaylistScreen} />
+        <Stack.Screen name="PlaylistName" component={PlaylistNameScreen} />
+        <Stack.Screen name="Finish" component={FinishScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
