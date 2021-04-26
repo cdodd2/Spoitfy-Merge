fe908f9d916af08921a13e0d0566dd4d72cf277a
cdodd2 <Chris@Christophers-MacBook-Pro-2.local>
2021-04-26 18:11:58 -0400

    added flatlist

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import * as React from 'react';
+import {useState} from 'react';
 import { Button, View, Text, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import SelectBox from 'react-native-multi-selectbox';
+import {xorBy} from 'lodash';
 
 function LoginScreen({ navigation }) {
   return (
@@ -25,19 +28,75 @@ function LoginScreen({ navigation }) {
   );
 }
 
-function PlaylistScreen({ navigation }) {
+
+function populatePlaylists() {
+  const []
+}
+
+function PlaylistScreen({navigation}) {
+
+const K_OPTIONS = [
+  {
+    item: 'Juventus',
+    id: 'JUVE',
+  },
+  {
+    item: 'Real Madrid',
+    id: 'RM',
+  },
+  {
+    item: 'Barcelona',
+    id: 'BR',
+  },
+  {
+    item: 'PSG',
+    id: 'PSG',
+  }
+]
+  const [selectedItems, setSelectedItems] = useState([])
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Playlists Available</Text>
-      <Button
-        title="Select Playlists"
-        onPress={() => navigation.navigate('PlaylistName')}
-      />
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <Text style={{textAlign: 'center'}}>Playlists Available</Text>
+      <View style={{margin: 30}}>
+      {K_OPTIONS.length ? (
+        <View>
+          <SelectBox
+          style={{padding: 10}}
+          label="Select multiple"
+          options={K_OPTIONS}
+          selectedValues={selectedItems}
+          onMultiSelect={onMultiChange()}
+          onTapClose={onMultiChange()}
+          toggleIconColor={'#1DB954'}
+          searchIconColor={'#1DB954'}
+          arrowIconColor={'#1DB954'}
+          multiOptionContainerStyle={{backgroundColor: '#1DB954'}}
+
+          isMulti
+          />
+          <Button
+          title="Select Playlists"
+          onPress={() => navigation.navigate('PlaylistName')}
+          />
+        </View>
+      ) : (
+        <Text style={{textAlign: 'center'}}>No Playlists Found!</Text>
+      )}
+      {selectedItems.map(select => <Text>{select.item}</Text>)}
+      </View>
     </View>
   );
+
+  function onMultiChange() {
+    return (item) => setSelectedItems(xorBy(selectedItems, [item], 'id'))
+  }
 }
 
 function PlaylistNameScreen({ navigation }) {
+
+  const [Name, setName] = useState("");
+
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Name your new playlist:</Text>
@@ -45,11 +104,14 @@ function PlaylistNameScreen({ navigation }) {
         style={{height: 40}}
         placeholder="Playlist Name"
         defaultValue={''}
+        value={Name}
+        onChangeText={setName}
       />
       <Button
         title="Enter Name"
         onPress={() => navigation.navigate('Finish')}
       />
+      <Text>{Name}</Text>
     </View>
   );
 }
